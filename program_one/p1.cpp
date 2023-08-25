ab6e2b3bc56e1f4e02d193c560cf695c0a565240
cqllysto <aidansamuelwright@gmail.com>
2023-08-25 02:12:33 +0100

    worked on the p1.cpp file, added functions: add, readAt, count

---
+++ program_one/p1.cpp
@@ -0,0 +1,65 @@
+// Andrew Chapuis & Aidan Wright
+// CSC 255
+// Program 1a
+// 08/24/2023
+
+#include <iostream>   // gets cin, cout, cerr
+#include <fstream>    // gets file functions of open and close
+#include <string.h>   // gets the string type, which is part of std namespace
+
+#include "p1.h"
+
+using namespace std;
+
+// Constructor
+stringList::stringList(int listCapacity){
+    this->listCapacity = listCapacity;
+    a = new string[listSize];
+}
+
+// Destructor
+stringList::~stringList(){
+    if(a){
+    delete []a;
+    a = NULL;
+    }
+}
+
+
+//******************************************************************************
+//Aidan Wright
+
+bool stringList::readAt(int index, string &text){
+    bool rc = FALSE;
+
+    if (index >= 0) && (index < listSize){
+        text = a[index];
+	
+	rc = TRUE;
+    }
+
+    return(rc);
+}
+
+//******************************************************************************
+//Aidan Wright
+
+int stringList::count(){
+   return(listSize)
+}
+
+
+//******************************************************************************
+//Aidan Wright
+
+bool stringList::add(string text){
+    bool rc = FALSE;    
+    a[listSize] = text;
+    
+    if (a[listSize] == text){
+        rc = TRUE;
+	listSize++;
+    }
+    return(rc);
+    
+}
