24f9a84861157a78413f2fc067286b999bf22326
chappy12345 <chapuis.andrew@gmail.com>
2023-09-18 14:00:57 -0500

    started p5h

---
+++ program_5/p5.h
@@ -0,0 +1,81 @@
+// Name: Andrew Chapuis and Aidan Wright
+// Team 3
+// Course: CSC 255
+// Program 5
+// Date: 09/18/23
+
+#ifndef __P5_H
+#define __P5_H
+
+#include <string>
+
+//******************************************************************************
+// Both
+
+// Declaration so that the class can be friended
+class sAVL;
+
+//******************************************************************************
+// Both
+
+class sNode {
+    private:
+	// Constructor for the node class	
+	sNode(std::string text = "");
+
+	// The variables that are associated with each node
+	std::string text;
+	int h;
+	sNode *left, *right;
+
+	friend class sAVL;
+};
+
+//******************************************************************************
+// Both
+
+class sAVL {
+    private:
+	// Tracks the number of nodes in the BST
+	int treeCount;
+	// Points to the root of the entire tree
+	sNode *root;
+
+	// Private function that finds the minimum value of a tree
+	std::string findMin(sNode *p);
+	// Private functions to be called by their public counterparts
+	// Inserts a node into the tree
+	bool insert(std::string text, sNode *&p);
+	// Removes a node from the tree
+	bool remove(std::string text, sNode *&p);
+	// Checks to see if text is in the BST
+	bool isIn(std::string text, sNode *p) const;
+	// Prints the tree from least to greatest
+	void printIt(sNode *p) const;
+	// Clears the tree
+	void clear(sNode *&p);
+	
+	void rotateLeft(sNode *&p1);
+	void rotateRight(sNode *&p1);
+	void bal(sNode *&p);
+	int height(sNode *p);
+	int calcHeight(sNode *p);
+
+    public:
+	// Constructor and destructor for the BST
+	sAVL();
+	~sAVL();
+
+	// Public functions that will call their private functions at the 
+	// root of the tree
+	bool insert(std::string text);
+	bool remove(std::string text);
+	bool isIn(std::string text) const;
+	void printIt() const;
+	void clear();
+	// Returns the number of nodes in the BST
+	int count() const;
+};
+
+#endif
+
