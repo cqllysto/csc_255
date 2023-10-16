808358d877b72531549b9ffbb85d2c946a24c76e
chappy12345 <chapuis.andrew@gmail.com>
2023-10-16 09:26:27 -0500

    Added comments and edited some functions

--- p7/p7.cpp
+++ p7/p7.cpp
@@ -89,7 +89,9 @@ bool intList::insertAt(int index, int key) {
 //******************************************************************************
 // Aidan Wright
 
-// this is a p1b.cpp problem :)
+// This will delete an entry from the array by shifting the remaining 
+// data to the left and will return the key that was in the deleted index
+// unless the index given is outside the bounds of the list created
 bool intList::deleteAt(int index, int &key) {
     bool rc = false;
     if ((index >= 0) && (index < listSize)) {
@@ -114,7 +116,8 @@ void intList::clear() {
 //******************************************************************************
 // Andrew Chapuis
 
-
+// This function will search the list until it finds the first instance of the
+// key given or it reaches the end of the list.
 int intList::getIndex(int key) const {
     int rc = -1;
     for (int i = 0; i < listSize; i++) {
@@ -148,85 +151,118 @@ int intList::count() const {
 }
 
 //******************************************************************************
-// Aidan Wright
+// Andrew Chapuis
 
-
+// Sorts the list by moving the maximum key in the array to the rightmost index
+// and then decreasing the amount of the array that is being looked at by 1 
+// each time
 void intList::bubbleSort() {
-    for (int i = 0; i < listSize; i++) {
-        for (int j = 0; j < listSize; j++) {
+    // As i is decreased, the maximum values that have already been sorted are
+    // not compared
+    for (int i = listSize - 1; i > 0 ; i--) {
+        for (int j = 0; j < i; j++) {
             if (a[j] > a[j + 1]) {
-                int temp = a[j];
-                a[j] = a[j + 1];
-                a[j + 1] = temp;
+                swap(a[j], a[j + 1]);
             }
         }
     }
 }
-
 
 //******************************************************************************
 // Aidan Wright
 
-// these probably wont work yet lol
+// Sorts the array by finding the index with the minimum key in the array 
+// and then swapping that index with the leftmost index that has not been sorted
 void intList::selectionSort() {
-    int min;
-    for (int i = 0; i < listSize; i++) {
-        min = i;
+    for (int i = 0; i < listSize - 1; i++) {
+	// Initialize minIndex to i each time the loop reiterates
+        int minIndex = i;
+	// All the keys in the array will be compared with minIndex to find 
+	// the index with the minimum value
         for (int j = i + 1; j < listSize; j++) {
-            if (a[j] < a[min]){
-                min = j;
+	    // If a smaller key was found, minIndex will be set to the index 
+	    // being compared
+            if (a[j] < a[minIndex]) {
+                minIndex = j;
             }
         }
-
-        // change this function to not use swap
-        if (min != i) {
-            swap (a[min], a[i]);
+	// If i is not the minIndex swap the values in the array
+        if (minIndex != i) {
+            swap (a[minIndex], a[i]);
         }
     }
 }
 
+//******************************************************************************
+// Andrew Chapuis
+
+// Sorts the list by slowly increasing the size of the sorted subarray. This is
+// accomplished by inserting the next index into the subarray and then swapping
+// that key with the index to the left's key if it is less than the key. Once it
+// is sorted, the subarray will add the next index
 void intList::insertionSort() {
-    for (int i = 1; i < listSize; i++) {
-        int key = a[i];
-        int j = i - 1;
-        while (j >= 0 && a[j] > key) {
-            a[j + 1] = a[j];
-            j = j - 1;
-        }
-        a[j + 1] = key;
+    // i keeps track of the sorted subarray
+    for (int i = 0; i < listSize - 1; i++) {
+	// j is initialized at the index being inserted into the subarray
+	for (int j = i + 1; j > 0; j--) {
+	    // The inserted index is swapped until it is in its sorted place 
+	    if (a[j - 1] > a[j]) {
+		swap(a[j - 1], a[j]);
+	    } else {
+		break;
+	    }
+	}
     }
 }
 
+//******************************************************************************
+// Aidan Wright
 
+// Prints a specific number of items on the list unless the passed in number is
+// greater than the size of the list
 void intList::printIt(int n) const {
+    // Print the entire list if n is greater than or equal to listSize
     if (n >= listSize) {
         for (int i = 0; i < listSize; i++) {
 	        cout << "At pos " << i << " there is " << a[i] << "\n";
         }
+    // Only n items will be printed from the list and then the last entry in the
+    // list will also be printed
     } else {
         for (int i = 0; i < n; i++) {
-	        cout << "At pos " << i << " there is " << a[i] << "\n";
-            if (i = n-1) {
-                cout << "At pos " << listSize-1 << " there is " << a[listSize-1] << "\n";
+	    cout << "At pos " << i << " there is " << a[i] << "\n";
+            // After the nth entry is printed, print the last entry on the list
+	    if (i == n - 1) {
+                cout << "At pos " << listSize - 1 << " there is " 
+		    << a[listSize - 1] << "\n";
             }
         }
     }
 }
 
+//******************************************************************************
+// Aidan Wright
 
+// Returns the capacity of the list
 int intList::capacity() {
     return(listCapacity);
 }
 
-//returns true if the list is sorted in ascending order
+//******************************************************************************
+// Andrew Chapuis
+
+// returns true if the list is sorted in ascending order
 // otherwise, it returns false 
 // If it fails, print the index where the failure occurs
-
 bool intList::isSorted() const {
     bool rc = true;
-    for (int i = 1; i < listSize; i++) {
-        if (a[i-1] > a[i]) {
+    for (int i = 0; i < listSize - 1; i++) {
+	// If an index's key is greater than the index + 1's key, then the index
+	// will be printed and the function will return false.
+        if (a[i] > a[i + 1]) {
             rc = false;
+	    cout << "Is not sorted at index " << i << "\n";
+	    break;
         }
     }
     return rc;
