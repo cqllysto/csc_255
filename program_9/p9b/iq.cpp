6a897c76098ce7b3d8415f06f8a85c8fbdc5205d
cqllysto <aidansamuelwright@gmail.com>
2023-11-07 14:07:06 -0600

    added data structures

---
+++ program_9/p9b/iq.cpp
@@ -0,0 +1,17 @@
+// Name: Andrew Chapuis and Aidan Wright
+// Team 3
+// Course: CSC 255
+// Program 9
+// Date: 11/1/23
+
+#ifndef __IQ_H
+#define __IQ_H
+
+#include "../../p3/p3.h"
+
+
+
+
+
+
+#endif
